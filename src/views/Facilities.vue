9406b5d0d0a91f95741e3c76aa391385df97369a
Ann Chemutai <annchemutai@Anns-MacBook-Pro.local>
2019-09-18 21:43:05 +0300

    further changes on design

--- src/views/Facilities.vue
+++ src/views/Facilities.vue
@@ -6,18 +6,61 @@
 
         <v-row > 
           <v-col class="lg12">
-            <v-btn class="primary">Add Facility</v-btn>
+            <v-btn class="warning">Add Facility</v-btn>
           </v-col>
         </v-row>
 
         <v-row>
           <v-col class="lg12">
-			     <v-data-table :headers="headers":items="orgunits" :items-per-page="5" class="elevation-1"></v-data-table>
+			     <v-data-table :headers="orgunits_headers":items="orgunits_list" :items-per-page="5" class="elevation-1">
+            <template slot="items" slot-scope="props">
+              <td>{{ props.item.county_name }}</td>
+              <td class="text-xs-left">{{ props.item.sub_county_name }}</td>
+              <td class="text-xs-left">{{ props.item.code }}</td>
+              <td class="text-xs-left">{{ props.item.name }}</td>
+              <td class="text-xs-left">{{ props.item.id }}</td>
+              <td class="text-xs-left"><v-btn color="blue lighten-2" dark @click = "dialog = true">
+            Click Me
+          </v-btn></td>
+            </template>   
+           </v-data-table>
           </v-col>
         </v-row>
-
-  	</v-container>
-  </div>
+<!--Dialog to view SDPs -->
+        <v-dialog
+        v-model="dialog"
+        width="500"
+      ><v-card>
+          <v-card-title
+            class="headline grey lighten-2"
+            primary-title
+          >
+            View SDPs in Facility
+          </v-card-title>
+  
+          <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :items-per-page="5"
+      class="elevation-1"
+    ></v-data-table>
+  
+          <v-divider></v-divider>
+  
+          <v-card-actions>
+            <div class="flex-grow-1"></div>
+            <v-btn
+              color="primary"
+              text
+              @click="dialog = false"
+            >
+              I accept
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      </v-container>
+    </div>
 </app-layout>
 </template>
 
@@ -30,11 +73,11 @@
       'app-layout': DefaultLayout,
     },
     mounted(){
-      this.getCounties()
+      this.getOrgunits()
     },
     data () {
     return {
-      headers: [
+      orgunits_headers: [
         {
           text: 'Name',
           align: 'left',
@@ -42,41 +85,94 @@
           value: 'county_name',
         },
         { text: 'Sub-County', value: 'subcounty_name' },
-        { text: 'Facility Code', value: 'mfl' },
+        { text: 'Facility Code', value: 'code' },
         { text: 'Facility Name', value: 'facility_name' },
-        { text: 'No of SDPs', value: 'no_of_sdps' },
+        { text: 'No of SDPs', value: 'id' },
+        { text: 'Action', value: 'id' },
       ],
-      orgunits: [
+      
+      orgunits_list: [],
+      dialog: false,
+      headers: [
         {
-          county_name: 'Frozen Yogurt',
-          subcounty_name: 159,
-          mfl: 6.0,
-          facility_name: 24,
-          no_of_sdps: 4.0,
+          text: 'Dessert (100g serving)',
+          align: 'left',
+          sortable: false,
+          value: 'name',
+        },
+        { text: 'Calories', value: 'calories' },
+        { text: 'Fat (g)', value: 'fat' },
+        { text: 'Carbs (g)', value: 'carbs' },
+        { text: 'Protein (g)', value: 'protein' },
+        { text: 'Iron (%)', value: 'iron' },
+      ],
+      desserts: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          iron: '1%',
         },
         {
-          county_name: 'Frozen Yogurt',
-          subcounty_name: 159,
-          mfl: 6.0,
-          facility_name: 24,
-          no_of_sdps: 4.0,
-        }
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          iron: '1%',
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          iron: '7%',
+        },
+        {
+          name: 'Cupcake',
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          iron: '8%',
+        },
+        {
+          name: 'Gingerbread',
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+          iron: '16%',
+        },
+        {
+          name: 'Jelly bean',
+          calories: 375,
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+          iron: '0%',
+        },
+        
       ],
-      orgunits_list: []
+
     }
     },
 
     methods: {
-      getCounties() {
+      getOrgunits() {
         //get api end point
-        apiCall({url: "question_per_checklist/1", method: "GET"})
-            .then(resp => {
-                console.log("Counties request", resp)
-                Vue.set(this.orgunits_list, "data", resp.data)
-            })
-            .catch(error => {
-                console.log("Error is ", error.response)
-            })
+        apiCall({url: "org_units", method: "GET"})
+            // .then(response => {
+            //     Vue.set(this.orgunits_list, "data", response.data.data)
+            //     console.log("Counties request", response)
+            // })
+            // .catch(error => {
+            //     console.log("Error is ", error.response)
+            // })
+            .then(response => (this.orgunits_list = response.data))
       },
 
     }
