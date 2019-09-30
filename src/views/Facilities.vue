b9acf34189b7f1876e2f07498db5e18428783bc5
Ann Chemutai <annchemutai@Anns-MacBook-Pro.local>
2019-09-30 17:18:03 +0300

    view sdp setup - incomplete

--- src/views/Facilities.vue
+++ src/views/Facilities.vue
@@ -19,48 +19,58 @@
               <td class="text-xs-left">{{ props.item.code }}</td>
               <td class="text-xs-left">{{ props.item.name }}</td>
               <td class="text-xs-left">{{ props.item.id }}</td>
-              <td class="text-xs-left"><v-btn color="blue lighten-2" dark @click = "dialog = true">
-            Click Me
-          </v-btn></td>
+              <td class="text-xs-left"><v-btn color="blue lighten-2" dark @click = "view_sdp_dialog = true">View SDP</v-btn></td>
+              <td class="text-xs-left"><v-btn color="green lighten-2" dark @click = "fill_qnr_dialog = true">Fill Questionnaire</v-btn></td>
             </template>   
            </v-data-table>
           </v-col>
         </v-row>
 <!--Dialog to view SDPs -->
-        <v-dialog
-        v-model="dialog"
-        width="500"
-      ><v-card>
-          <v-card-title
-            class="headline grey lighten-2"
-            primary-title
-          >
-            View SDPs in Facility
-          </v-card-title>
+      <v-dialog v-model="view_sdp_dialog" width="500" >
+        <v-card>
+          <v-card-title class="headline grey lighten-2" primary-title> View SDPs in Facility </v-card-title>
   
-          <v-data-table
-      :headers="headers"
-      :items="desserts"
-      :items-per-page="5"
-      class="elevation-1"
-    ></v-data-table>
+          <v-data-table :headers="headers" :items="desserts" :items-per-page="5" class="elevation-1" ></v-data-table>
   
           <v-divider></v-divider>
   
           <v-card-actions>
             <div class="flex-grow-1"></div>
-            <v-btn
-              color="primary"
-              text
-              @click="dialog = false"
-            >
-              I accept
-            </v-btn>
+            <v-btn color="primary" text @click="dialog = false" >  Add New SDP </v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
-      </v-container>
-    </div>
+
+      <!--Dialog to fill questionnaire -->
+      <v-dialog v-model="fill_qnr_dialog" width="500" >
+        <v-card>
+          <v-card-title class="headline grey lighten-2" primary-title> Choose SDP </v-card-title>
+          <v-form v-model="valid">
+            <v-row>
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <v-radio-group class="row" v-model ="choose_sdp">
+              <div v-for="sdp in sdps_list">
+                <v-radio :value = "sdp.id" :label="sdp.name"></v-radio>    
+               </div>
+            </v-radio-group>
+          </v-col></v-row>
+          </v-form>
+  {{choose_sdp}}
+  
+          <v-divider></v-divider>
+  
+          <v-card-actions>
+            <div class="flex-grow-1"></div>
+            <v-btn color="red" dark @click="fill_qnr_dialog = false" > Cancel </v-btn>
+            <v-btn color="primary" dark to="htc_questionnaire" @click="dialog = false" > Go </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-container>
+  </div>
 </app-layout>
 </template>
 
@@ -74,6 +84,7 @@
     },
     mounted(){
       this.getOrgunits()
+      this.getSDPs()
     },
     data () {
     return {
@@ -92,7 +103,11 @@
       ],
       
       orgunits_list: [],
+      sdps_list: [],
+      choose_sdp: '',
       dialog: false,
+      fill_qnr_dialog : false,
+      view_sdp_dialog:false,
       headers: [
         {
           text: 'Dessert (100g serving)',
@@ -165,6 +180,10 @@
       getOrgunits() {
         //get api end point
         apiCall({url: "org_units", method: "GET"}).then(response => (this.orgunits_list = response.data))
+      },
+      getSDPs() {
+        //get api end point
+        apiCall({url: "get_sdps", method: "GET"}).then(response => (this.sdps_list = response.data))
       },
 
     }
