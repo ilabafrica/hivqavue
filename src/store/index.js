bc482ab145a82c8f56aa5671e901cbedafd204b0
kenmutuma <kennethsila2@gmail.com>
2020-01-07 11:32:12 +0300

    CASL

--- src/store/index.js
+++ src/store/index.js
@@ -3,8 +3,10 @@ import Vuex from 'vuex'
 import user from './modules/user'
 import auth from './modules/auth'
 import accesscontrol from './modules/accesscontrol'
+import { abilitiesPlugin } from '@casl/vue'
 
 Vue.use(Vuex)
+Vue.use (abilitiesPlugin)
 
 const debug = process.env.NODE_ENV !== 'production'
 
